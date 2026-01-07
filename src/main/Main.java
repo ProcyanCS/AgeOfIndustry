96891f45776db0226f5e03827c58b8013cee17de
ProcyanCS <jimmy.price.iii@gmail.com>
2026-01-07 15:06:16 -0700

    Inital commit with eclipse configuration after moving to git repo.

---
+++ src/main/Main.java
@@ -0,0 +1,17 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		JFrame mainWindow = new JFrame("Age of Industry");
+		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainWindow.setResizable(true);
+		
+		
+
+	}
+
+}
